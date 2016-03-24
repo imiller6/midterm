a33713a9026748d8a3d96bdb9bcfdfe8bcac5f70
Ian Miller <imiller6@mail.valenciacollege.edu>
2016-03-23 23:27:24 -0400

    q1

--- q1.py
+++ q1.py
@@ -1,9 +1,7 @@
- 
-
 def computeMinimumPayment( balance ):
-    if int(balance) * .021 > 10:
-    return int(balance) * .021
-elif int(balance) == 10:
-    return 10
-else:
-    return int(balance)
+	if int(balance) * .021 > 10:
+    		return int(balance) * .021
+        elif int(balance) == 10:
+    		return 10
+        else:
+    		return int(balance)
